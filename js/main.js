667550ebdd5af8dbfea838e7cf3c6e297d8b441d
Nicholas Zehl <njz2587@g.rit.edu>
2022-08-29 00:43:16 -0400

    30fps?

--- js/main.js
+++ js/main.js
@@ -370,7 +370,7 @@ function setup() {
 		if (paused) return; // keep this commented out for now
 		
 		// #1 - Calculate "delta time"
-		let dt = 1/60;
+		let dt = 1/30;
 		if (dt > 1/12) dt=1/12;						
 			
 		// #2 - Move bear
